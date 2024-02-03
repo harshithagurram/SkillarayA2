c2ddeb7709686649cabf640818b16e059c0ce0be
Harshitha <Harshitha@Harshitha>
2024-02-03 13:32:02 +0530

    add comment to add category

--- src/test/java/testScripts/AddCategoryTest.java
+++ src/test/java/testScripts/AddCategoryTest.java
@@ -7,7 +7,7 @@ import org.testng.asserts.SoftAssert;
 
 import genericUtilities.BaseClass;
 import genericUtilities.Iconstantpath;
-
+//this test verifies if user is able to create category
 public class AddCategoryTest extends BaseClass {
 	@Test
 	public void addCategoryTest() throws InterruptedException {
